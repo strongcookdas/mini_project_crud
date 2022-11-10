6da1423b0bf6da8ae7510c0c4b583f23df896b16
strongcookdas <goodjav141592@gmail.com>
2022-11-10 11:07:35 +0900

    build : start mini project crud

---
+++ src/main/java/com/example/mini_project_crud_exercise/MiniProjectCrudExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_crud_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectCrudExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectCrudExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mini_project_crud_exercise/MiniProjectCrudExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_crud_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectCrudExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
